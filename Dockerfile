b5166e5c3095c97837292f27118253e5389514ab
bpiskorski <bpiskor97@gmail.com>
2025-02-17 17:42:48 +0100

    added some functionality to sys_stats

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /home
 
 RUN apt update 
 RUN apt install -y nano 
-
+RUN apt install -y man-db manpages-posix
 
 #to run in cmd:
 #docker exec -it ubuntu-container bash
--- scripts/sys_stats.sh
+++ scripts/sys_stats.sh
@@ -2,7 +2,8 @@ user=$(whoami)
 date=$(date)
 path=$(pwd)
 kern=$(uname -v)
-
+memorytotal=$(free -m | awk '/m/ {print $2}')
+memoryused=$(free -m | awk '/m/ {print $3}')
 showuptime(){
     up=$(uptime -p | cut -c4-)
     cat << EOF
@@ -12,30 +13,61 @@ This machine has been up for ${up}
 EOF
 }
 
+calculatememory(){
+   # mem=$("(memoryused / memorytotal) * 100 | bc")
+    cat << EOF
+    ---
+    You have used $memoryused mb of $memorytotal mb
+    ---
+EOF
+
+}
+listallusers(){
+    awk -F ":" '{print $1}' /etc/passwd # F to check for text separator
+}
+printallshells(){
+    awk -F "/" '/^\// {print $NF}' /etc/shells # check if line start with / and NF is last field
+}
+
+
 start(){
     cat<< EOF
     ---
-    Hello {$user}, choose one to continue:
+    Signed in as $user
+    Choose one to continue:
     1. Uptime
     2. Free memory
     3. Backup
-    4. Exit
+    4. List all users
+    5. List all shells
+    0. Exit
     ---
 EOF
     read x
 
     case $x in
+   0)
+        exit 0
+        ;;
     1)
         showuptime
+        start
         ;;
     2)
-        free -m
+        calculatememory
+        start
         ;;
     3) 
         echo "not developed yet"
+        start
         ;;
-    4)
-        exit 0
+    4) echo "list all usrs "
+        listallusers
+        start
+        ;;
+    5) echo "print all shells"
+        printallshells
+        start
         ;;
     
     *)
