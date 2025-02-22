5f5df7fbeefe02374a2af277f210e4bffb511876
bpiskorski <bpiskor97@gmail.com>
2025-02-23 00:53:22 +0100

    configured mysql to start from host docker compose, implemented database connection and basic configuration in playbook

---
+++ scripts/djangoProject/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'db', 
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'mysql',  
+        'PORT': '3306', 
+    }
+}
